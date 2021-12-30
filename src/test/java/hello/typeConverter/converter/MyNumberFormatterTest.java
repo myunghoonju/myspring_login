5686f53238b44eb69c1e09c3b46896dc39a8dfc2
myunghoonju <myunghoonju@gmail.com>
2021-12-31 00:35:25 +0900

    Formatter test

---
+++ src/test/java/hello/typeConverter/converter/MyNumberFormatterTest.java
@@ -0,0 +1,29 @@
+package hello.typeConverter.converter;
+
+import hello.login.web.typeConverter.formatter.MyNumberFormatter;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MyNumberFormatterTest {
+
+    MyNumberFormatter myNumberFormatter = new MyNumberFormatter();
+
+    @Test
+    void parse() throws ParseException {
+        Number result = myNumberFormatter.parse("1,000", Locale.KOREA);
+
+        assertThat(result).isEqualTo(1000L);
+    }
+
+    @Test
+    void print() {
+        String result = myNumberFormatter.print(1_000, Locale.KOREA);
+
+        assertThat(result).isEqualTo("1,000");
+
+    }
+}
