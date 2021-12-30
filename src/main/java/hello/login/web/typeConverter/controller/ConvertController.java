e626072aa089fb39e9fb9de00eedc949d316ad1b
myunghoonju <myunghoonju@gmail.com>
2021-12-31 00:21:16 +0900

    converter with view template(thymleaf)  - th:filed  - {{}}, {} diff

--- src/main/java/hello/login/web/typeConverter/controller/ConvertController.java
+++ src/main/java/hello/login/web/typeConverter/controller/ConvertController.java
@@ -1,23 +1,56 @@
 package hello.login.web.typeConverter.controller;
 
+import hello.login.web.typeConverter.type.Form;
 import hello.login.web.typeConverter.type.IpPort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class ConvertController {
 
+    @ResponseBody
     @GetMapping("/api/v2")
     public String convert(@RequestParam Integer data) {
         System.out.println("data = " + data);
         return "ok";
     }
 
+    @ResponseBody
     @GetMapping("/api/ipPort")
     public String convert2(@RequestParam IpPort ipPort) {
         System.out.println("ipPort ip = " + ipPort.getIp());
         System.out.println("ipPort port = " + ipPort.getPort());
         return "ok";
     }
+
+    @GetMapping("/api/converter-view")
+    public String converterView(Model model) {
+        model.addAttribute("number", 10_000);
+        model.addAttribute("ipPort", new IpPort("127.0.0.1", 8080));
+        return "converters/converter-view";
+    }
+
+    @GetMapping("/api/converter-edit")
+    public String converterForm(Model model) {
+        IpPort ipPort = new IpPort("127.0.0.1", 8080);
+        Form form = new Form(ipPort);
+        model.addAttribute("form", form);
+
+        return "converters/converter-form";
+    }
+
+    @PostMapping("/api/converter-edit")
+    public String converterEdit(@ModelAttribute Form form, Model model) {
+        IpPort ipPort = form.getIpPort();
+        model.addAttribute("ipPort", ipPort);
+
+        return "converters/converter-view";
+    }
+
+
 }
---
+++ src/main/java/hello/login/web/typeConverter/type/Form.java
@@ -0,0 +1,12 @@
+package hello.login.web.typeConverter.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class Form {
+
+    private IpPort ipPort;
+}
