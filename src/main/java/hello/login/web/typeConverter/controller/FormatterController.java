27c431d78a4c9edbebe8924502eb5196b19d1a45
myunghoonju <myunghoonju@gmail.com>
2021-12-31 01:04:52 +0900

    add spring support formatter

---
+++ src/main/java/hello/login/web/typeConverter/controller/FormatterController.java
@@ -0,0 +1,30 @@
+package hello.login.web.typeConverter.controller;
+
+import hello.login.web.typeConverter.type.Form;
+import hello.login.web.typeConverter.type.FormatterForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class FormatterController {
+
+    @GetMapping("/api/formatter/edit")
+    public String formatterForm(Model model) {
+        FormatterForm form = new FormatterForm();
+        form.setNumber(10000);
+        form.setLocalDateTime(LocalDateTime.now());
+        model.addAttribute("form", form);
+
+        return "converters/formatter-form";
+    }
+
+    @PostMapping("/api/formatter/edit")
+    public String formatterEdit(@ModelAttribute("form") FormatterForm form) {
+        return "converters/formatter-view";
+    }
+}
---
+++ src/main/java/hello/login/web/typeConverter/type/FormatterForm.java
@@ -0,0 +1,19 @@
+package hello.login.web.typeConverter.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.NumberFormat;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class FormatterForm {
+
+    @NumberFormat(pattern = "###,###")
+    private Integer number;
+
+    @NumberFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime localDateTime;
+}
