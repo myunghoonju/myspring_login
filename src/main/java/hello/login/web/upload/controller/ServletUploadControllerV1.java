648329347cbd9564f97821511a86ed10f82d3198
myunghoonju <myunghoonju@gmail.com>
2021-12-31 11:44:25 +0900

    servlet file upload.

--- src/main/java/hello/login/web/upload/controller/ServletUploadControllerV1.java
+++ src/main/java/hello/login/web/upload/controller/ServletUploadControllerV1.java
@@ -1,7 +1,10 @@
 package hello.login.web.upload.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +13,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 
 @Slf4j
 @Controller
 @RequestMapping("/api/servlet/v1")
 public class ServletUploadControllerV1 {
+
+    @Value("${file.dir}")
+    private String fileDir;
 
     @GetMapping("/upload")
     public String newFile() {
@@ -30,6 +38,31 @@ public class ServletUploadControllerV1 {
         log.info("itemName = {}", itemName);
         Collection<Part> parts = request.getParts();
         log.info("parts = {}", parts);
+
+        for (Part part : parts) {
+            log.info("==========");
+            log.info("name = {}", part.getName());
+            Collection<String> headerNames = part.getHeaderNames();
+            for (String headerName : headerNames) {
+                log.info("header = {}: {}", headerName, part.getHeader(headerName));
+            }
+            //content-disposition
+            log.info("submittedFileName= {} ", part.getSubmittedFileName());
+            //get size
+            log.info("size = {}", part.getSize());
+            //read data
+            InputStream inputStream = part.getInputStream();
+            String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            log.info("body = {}", body);
+
+            //save file
+            if (StringUtils.hasText(part.getSubmittedFileName())) {
+                String fullPath = fileDir + part.getSubmittedFileName();
+                log.info("save file dir = {}", fullPath);
+                part.write(fullPath);
+            }
+
+        }
 
         return "uploading/upload-form";
     }
