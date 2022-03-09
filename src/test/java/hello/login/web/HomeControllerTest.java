79cebfcab7d94fd2e0d10b41b638e084c3a6b053
JU MYUNGHOON <myunghoonju@gmail.com>
2022-03-10 00:21:59 +0900

    add test properties

--- src/test/java/hello/login/web/HomeControllerTest.java
+++ src/test/java/hello/login/web/HomeControllerTest.java
@@ -4,17 +4,25 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest
 class HomeControllerTest {
+
+    @Value("${foo}")
+    String foo;
 
     @Autowired
     MockMvc mockMvc;
@@ -29,5 +37,10 @@ class HomeControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/hometest").accept(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(Matchers.equalTo("loginHome")));
+    }
+
+    @Test
+    void getProperties() {
+        assertThat(foo).isEqualTo("bar");
     }
 }
